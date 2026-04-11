419c033b012b9af1a9fc590f29480e5c11f7fa85
Felipe-MacielCS <122392496+Felipe-MacielCS@users.noreply.github.com>
2026-04-11 14:44:23 -0500

    Add Goat Language Core Funcitons

---
+++ goat.py
@@ -0,0 +1,117 @@
+import sys
+
+# read the argumest, interpret from this file
+program_filepath = sys.argv[1]
+
+# TOKENIZATION -------------------------------------------------------------------------------------------------------------------
+
+# read lines
+program_lines = []
+with open(program_filepath, "r") as program_file:
+    program_lines = [line.strip() for line in program_file.readlines()]
+
+program = [] # list of instructions
+token_counter = 0 # keep tracks where we are in the program in parsing
+label_tracker = {} # keep tracks of which index in our program list a label points to
+
+for line in program_lines:
+    parts = line.split(" ")
+    opcode = parts[0]
+
+    # check for empty line 
+    if opcode == "":
+        continue
+
+    # check if it is a label
+    if opcode.endswith(":"):
+        label_tracker[opcode[:-1]] = token_counter
+        continue
+
+    # store opcode token
+    program.append(opcode)
+    token_counter += 1
+
+    # handle each opcode
+    if opcode == "EAT": # PUSH
+        # expects a number
+        number = int(parts[1])
+        program.append(number)
+        token_counter += 1
+    elif opcode == "BLEAT": # PRINT
+        # parse string literal (it might have spaces, so we add the rest of the string and remove "")
+        string_literal = " ".join(parts[1:])[1:-1]
+        program.append(string_literal)
+        token_counter += 1
+    elif opcode == "CHECK_FEET": #JUMP.EQ.0
+        # reads the label
+        label = parts[1]
+        program.append(label)
+        token_counter += 1
+    elif opcode == "CHECK_HORNS": # JUMP.GT.0
+        # reads the label
+        label = parts[1]
+        program.append(label)
+        token_counter += 1    
+
+# Interpret -------------------------------------------------------------------------------------------------------------------
+
+class Stack:
+
+    def __init__(self, size):
+        self.buf = [0 for _ in range(size)]
+        self.sp = -1
+
+    def push(self, number):
+        self.sp += 1 # sp = stack pointer
+        self.buf[self.sp] = number
+
+    def pop(self, number):
+        number = self.buf[self.sp]
+        self.sp -= 1
+        return number
+    
+    # helper function, it does not take any arguments, it returns the top value of the stack without moving the stack pointer
+    def top(self):
+        return self.buf[self.sp]
+    
+pc = 0
+stack = Stack (256)
+
+while program[pc] != "SLAUGHTER": # Terminates
+    opcode = program[pc]
+    pc += 1
+ 
+    if opcode == "EAT": # PUSH
+        number = program[pc]
+        pc += 1
+
+        stack.push(number)
+    elif opcode == "POOP": # POP
+        stack.pop()
+    elif opcode == "LACTATE": # ADD
+        a = stack.pop()
+        b = stack.pop()
+        stack.push(a+b)
+    elif opcode == "SHEAR": # SUB
+        a = stack.pop()
+        b = stack.pop()
+        stack.push(b-a)
+    elif opcode == "BLEAT": # PRINT
+        string_literal = program[pc]
+        pc += 1
+        print(string_literal)
+    elif opcode == "ABSORB": # READ
+        number = int(input())
+        stack.push(number)
+    elif opcode == "CHECK FEET": # JUMP.EQ.0
+        number = stack.top()
+        if number == 0:
+            pc = label_tracker[program[pc]]
+        else:
+            pc += 1
+    elif opcode == "CHECK HORNS": # JUMP.GT.0
+        number = stack.top()
+        if number > 0:
+            pc = label_tracker[program[pc]]
+        else:
+            pc += 1
